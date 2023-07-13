cd8643fd9ffc91db3467ea8a45798d4222dd0a50
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-07-13 13:15:51 +0700

    Initial Commit

---
+++ PersonCounterIOS/PersonCounterIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCounterIOSApp.swift
+//  PersonCounterIOS
+//
+//  Created by Rainer Regan on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonCounterIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
